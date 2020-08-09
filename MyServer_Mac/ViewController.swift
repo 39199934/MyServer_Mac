b50b82a310359555c3c317257fbdb27c84f8714d
rolodestar <39199934@qq.com>
2020-08-09 20:00:51 +0800

    Initial Commit

---
+++ MyServer_Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyServer_Mac
+//
+//  Created by rolodestar on 2020/8/9.
+//  Copyright © 2020 Rolodestar Studio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
